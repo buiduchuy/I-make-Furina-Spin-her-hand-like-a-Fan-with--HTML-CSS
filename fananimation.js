bcfa27e2aa8b61d1997d16bf4cc48ad960df87ca
buiduchuy <huyxhhz@gmail.com>
2024-08-10 21:47:07 +0700

    Add files via upload

---
+++ fananimation.js
@@ -0,0 +1,16 @@
+let a = document.getElementById("img");
+function fanon() {
+    a.style.animationDuration = "3s";
+}
+function fanoff() {
+    a.style.animationDuration = "0s";
+}
+function fanon1() {
+    a.style.animationDuration = "1s";
+}
+function fanon2() {
+    a.style.animationDuration = "0.5s";
+}
+function fanon3() {
+    a.style.animationDuration = "0.1s";
+}
